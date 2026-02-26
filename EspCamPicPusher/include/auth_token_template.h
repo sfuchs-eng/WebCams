2d766610c245d3d0960a9639cfd9312a656d2aac
Stefan Fuchs <sfuchs@sfuchs.ch>
2026-02-26 21:45:22 +0100

    fixes to fit my provider

--- EspCamPicPusher/include/auth_token_template.h
+++ EspCamPicPusher/include/auth_token_template.h
@@ -2,7 +2,7 @@
 #define AUTH_TOKEN_H
 
 // Configuration of the receiving server endpoint and authentication token for secure image upload
-const char* SERVER_URL = "https://your-server.com/api/upload";
+const char* SERVER_URL = "https://your-server.com/upload.php";
 const char* AUTH_TOKEN = "your_secret_token_here";
 
 #endif // AUTH_TOKEN_H
